3dc76caeb3f384d221556a6a87e3f973cddfff93
leemurking <jamesgrant964@gmail.com>
2021-10-12 11:20:18 +0300

    Add the login page, splashscreen and signup page

---
+++ app/src/main/java/com/leemurking/legit_kingshealthcaretracker/RegisterActivity.kt
@@ -0,0 +1,13 @@
+package com.leemurking.legit_kingshealthcaretracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+
+    }
+}
